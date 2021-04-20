578c5a82a68c6f3c04f48459c77973eeb9eab34b
MrNobody503 <81288171+MrNobody503@users.noreply.github.com>
2021-04-20 21:10:13 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -244,11 +244,11 @@ async def echo(bot, update):
                 "video", format_id, format_ext)
             inline_keyboard.append([
                 InlineKeyboardButton(
-                    "🎞️SVideo🎞️",
+                    "🎞SVideo",
                     callback_data=(cb_string_video).encode("UTF-8")
                 ),
                 InlineKeyboardButton(
-                    "🗂️DFile🗂️",
+                    "📁DFile",
                     callback_data=(cb_string_file).encode("UTF-8")
                 )
             ])
